7ac3f7daa88a22f5567498f96616be969364e2c9
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-28 12:22:31 -0400

    adds logout function

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import logout
 from django.urls import path, include
-from .api import RegisterAPI, LoginAPI
+from .api import RegisterAPI, LoginAPI, UserAPI
+from .views import Logout
 
 urlpatterns = [
     path('api/auth/register', RegisterAPI.as_view()),
-    path('api/auth/login', LoginAPI.as_view())
+    path('api/auth/login', LoginAPI.as_view()),
+    path('api/auth/user', UserAPI.as_view()),
+    path('api/auth/logout', Logout.as_view())
 ]
