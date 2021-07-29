d584f5f5945ce297094fb75844cda6a00c746a00
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-07-29 15:44:23 -0400

    updates wine model

--- wines/models.py
+++ wines/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -7,10 +8,11 @@ from django.contrib.auth.models import User
 class Wine(models.Model):
     title = models.CharField(max_length=100)
     winery = models.CharField(max_length=100)
-    price = models.IntegerField
-    date_consumed = models.DateField
+    img = models.CharField(max_length=1000, blank=True)
+    price = models.IntegerField(default=0)
+    date_consumed = models.DateField(default=date.today)
     review = models.CharField(max_length=500, blank=True)
-    rating = models.FloatField
+    rating = models.FloatField(default=5)
     variety = models.CharField(max_length=100, blank=True)
     country = models.CharField(max_length=100)
     region = models.CharField(max_length=100, blank=True)
